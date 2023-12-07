ab9432a4816c37aac40b3a38305a21423644bfb9
ctwjh <ctwjh@goodcen.com>
2023-12-07 14:36:05 +0900

    project add

---
+++ src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+
+    public String Solution (String [] participation, String [] completion) {
+        String answer = "11";
+
+        HashMap<String, Integer> aa = new HashMap<>();
+
+
+
+
+        return answer;
+    }
+
+    public static void main(String [] args){
+        System.out.println("main start");
+        String [] part = {"1","2","3"};
+        String [] comp = {"1","2"};
+
+        Solution sol = new Solution();
+        sol.Solution(part, comp);
+    }
+
+
+
+}
